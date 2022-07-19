5e0a3628a7ce9981a8608b6baf173cea4f6c8d0e
rmlo <rmlomoreira@gmail.com>
2022-07-19 15:18:49 -0300

    Desenvolvimento dos layouts da listagem e detalhes das partidas

---
+++ app/src/main/java/br/com/rmlo/apptest/DetailActivity.kt
@@ -0,0 +1,19 @@
+package br.com.rmlo.apptest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.rmlo.apptest.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
--- app/src/main/java/br/com/rmlo/apptest/MainActivity.kt
+++ app/src/main/java/br/com/rmlo/apptest/MainActivity.kt
@@ -1,11 +1,16 @@
 package br.com.rmlo.apptest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.rmlo.apptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
